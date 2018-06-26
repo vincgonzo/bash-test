a78d4427ba53229fa7faade24d122b8da3bc496d
vincent-castanie <vincgonzo@hotmail.fr>
2018-06-26 08:30:41 +0200

    bash script to count and ordinate letter count in fiel text

--- dico/langstat.sh
+++ dico/langstat.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-filename='hello'
+# un petit display pour savoir quel est le param de départ
+echo -e "Vous avez lancé $0, il y a $# paramètres\nLe paramètre 1 est $1"
 
-echo "this is test : ${filename}"
-
-echo "Vous avez lancé $0, il y a $# paramètres"
-echo "Le paramètre 1 est $1"i
-
-char='A'
-
-filename='dico.txt'
-echo Start
-while read p; do
-    echo $p
-done < $filename
+# condition de vérification que le fichier exist et ai l'extension .txt
+if [ ! -e $1 ] || [ !"$1" == "*.txt" ];then
+	echo -e 'Un fichier text est requis (extension .txt) pour faire fonctionner le script\nEssayer à nouveau svp'
+else
+	# display result
+	awk -v FS= '{for (i=1;i<=NF;i++) a[tolower($i)]++;};
+		END {chars="abcdefghijklmnopqrstuvwxyz";
+		for (i=1;i<27;i++) { key=substr(chars,i,1);print a[key] " - " toupper(key) };}' $1 | sort -nr
+fi
